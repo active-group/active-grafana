abc46cb4207747c065fc32020c4a8f4ce0c87490
Christina Zeller <christina.zeller@active-group.de>
2023-12-06 15:56:29 +0100

    test: remove dummy-test

--- test/active_grafana/core_test.clj
+++ test/active_grafana/core_test.clj
@@ -1,8 +1,3 @@
 (ns active-grafana.core-test
   (:require [clojure.test :refer :all]
-            [active-grafana.core :refer :all]))
-
-(deftest a-test
-  (testing "I'm a happy test."
-    (is (= (foo "Active")
-           nil))))
+            [active-grafana.core :as core]))
