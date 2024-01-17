aac489f90484046fa0802b1f6d398840be343288
Christina Zeller <christina.zeller@active-group.de>
2024-01-17 09:53:39 +0100

    project: adjust artifact name

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject active-grafana "0.1.0-SNAPSHOT"
+(defproject de.active-group/active-grafana "0.1.0-SNAPSHOT"
   :description "Active Grafana: Helper to handle Grafana."
   :url "https://github.com/active-group/active-grafana"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
