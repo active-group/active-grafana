866d7e77235dc337e02a46512a10e42a0d1599f1
Christina Zeller <christina.zeller@active-group.de>
2024-02-12 09:48:42 +0100

    github-actions: prerelease-workflow

--- project.clj
+++ project.clj
@@ -3,19 +3,23 @@
   :url "https://github.com/active-group/active-grafana"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clj-commons/clj-http-lite "1.0.13"]
+  :dependencies [[org.babashka/http-client "0.3.11"]
+                 [org.clj-commons/clj-http-lite "1.0.13"]
                  [org.clojure/clojure "1.11.1"]
                  [org.clojure/data.json "2.4.0"]
                  [org.clojure/tools.cli "1.0.219"]
-                 [org.babashka/http-client "0.3.11"]
-                 [com.github.clj-easy/graal-build-time "1.0.5"]
+                 ; only needed for pod
                  [nrepl/bencode "1.1.0"]
                  [instaparse/instaparse  "1.4.12"]
-                                        ; 1.0.333
+                                           ; 1.0.333
                  [com.cognitect/transit-clj "1.0.329"]
-                 ]
-  ; This project has two main:
-  :main active-grafana.pod
-  ; :main active-grafana.main-creation
-  :profiles {:uberjar {:aot :all}}
-  )
+                 ; only needed for graalvm
+                 [com.github.clj-easy/graal-build-time "1.0.5"]]
+  :profiles {:uberjar   {:aot :all}
+
+             :as-pod    {:main active-grafana.pod
+                         :uberjar-name "active-grafana-pod.jar"}
+             :as-copy   {:main active-grafana.main-copy
+                         :uberjar-name "active-grafana-copy.jar"}
+             :as-adjust {:main active-grafana.main-adjust
+                         :uberjar-name "active-grafana-adjust.jar"}})
