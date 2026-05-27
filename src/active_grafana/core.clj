8cb63a4b44542d35c01e75c35e86c857c15ddd21
Tobias Paulus <tobias.paulus@active-group.de>
2026-05-27 16:40:22 +0200

    core(convenient-copy): Copy library-panels+alerts.

--- src/active_grafana/core.clj
+++ src/active_grafana/core.clj
@@ -101,9 +101,9 @@
 
         ;; go through all panels and search for "libraryPanel" entries
         ;; (contains nil for every non-library-panel)
-        panel-uids (remove nil? (map (fn [panel]
-                                       (get-in panel ["libraryPanel" "uid"]))
-                                     (get-in dashboard ["dashboard" "panels"])))]
+        panel-uids (->> (get-in dashboard ["dashboard" "panels"])
+                        (map (fn [panel] (get-in panel ["libraryPanel" "uid"])))
+                        (remove nil?))]
     ;; get cannot be nil - since the dashboard points to this library-panel
     (map (fn [panel-uid] (get (helper/json->clj
                                (api/get-library-element-by-uid (-> grafana-instance :url)
@@ -276,13 +276,15 @@
     ;; Note: inefficient to run the available-panels within copy-panel for every
     ;; panel within copy-panels
     ;; However, if the panels-list contains duplicates, we can handle it.
-  (let [available-panel-uids (map (fn [panel] (get panel "uid"))
+  (let [panel-uid (get panel "uid")
+        available-panel-uids (map (fn [available-panel] (get available-panel "uid"))
                                   (get-in (helper/json->clj
                                            (api/get-library-panels (-> grafana-instance :url)
                                                                    (-> grafana-instance :token)))
                                           ["result" "elements"]))
-        panel-uid (get panel "uid")
-        adjusted-panel (assoc (dissoc (dissoc (dissoc panel "id") "folderId") "meta") "folderUid" folder-uid)]
+        adjusted-panel (-> panel
+                           (dissoc "id" "folderId" "meta")
+                           (assoc "folderUid" folder-uid))]
     (if (some #(= panel-uid %) available-panel-uids)
       ;; before we can update the panel
       ;; we need to have its most recent version and put this version in the patch
@@ -293,7 +295,7 @@
                                   ["result" "version"])]
         (api/update-library-element (-> grafana-instance :url)
                                     (-> grafana-instance :token)
-                                    (get panel "uid")
+                                    panel-uid
                                     (helper/clj->json (assoc adjusted-panel "version" panel-version))))
       (api/create-library-element (-> grafana-instance :url)
                                   (-> grafana-instance :token)
@@ -344,8 +346,18 @@
 (defn- title= [title m]
   (= title (get m "title")))
 
+;; TODO: An idea to resolve the ambiguity could be
+;; to do a dry run of the whole thing persisting the
+;; chosen dashboard and folder uids
+;; we need to run the real thing without support
+;; of a user. Or without the need to be be an
+;; interactive program.
+
 (defn choose-dashboard-metadata
-  "TODO: document this fn"
+  "Searches a dashboard on a given [[grafana-instance]] by using a given
+   [[dashboard-title]] as query string returning the dashboard metadata, if the
+   search yields an unambiguous result.  If the search yields an ambiguous or no
+   result at all, this function throws an Exception."
   [grafana-instance dashboard-title]
   (let [dashboard-candidates       (->> dashboard-title
                                         (api/find-dashboards-by-query
@@ -368,6 +380,8 @@
 
           dashboard-ambiguous?
           (do
+            ;; TODO: We need a convenient way to choose the
+            ;; dashboard to copy or to refine the search
             (pprint/print-table ["title" "uid" "url" "description" "folderTitle" "folderUrl"]
                                 dashboard-candidates)
             (throw (ex-info (str "More than one (" dashboard-candidates-count
@@ -385,7 +399,11 @@
                            :dashboard-candidates             dashboard-candidates})))))
 
 (defn choose-folder-uid
-  "TODO: document this fn"
+  "Searches a folder on a given [[grafana-instance]] by using a given
+  [[folder-title]] as query string returning a folder uid, if the search yields
+  an unambiguous result. If the search yields no result at all this function
+  creates a folder with the title [[folder-title]] and returns its uid. If the
+  search yields an ambiguous result, this function throws an exception."
   [grafana-instance folder-title]
   (let [folder-candidates       (->> folder-title
                                      (api/find-folders-by-query (:url grafana-instance)
@@ -407,6 +425,10 @@
 
           folder-ambiguous?
           (do
+            ;; TODO: we need a more convenient way to resolve
+            ;; the ambiguity of the folder-ccandidates
+            ;; the user should be able to choose
+            ;; a folder conveniently
             (pprint/print-table ["title" "uid" "url"]
                                 folder-candidates)
             (throw (ex-info (str "More than one (" folder-candidates-count
@@ -423,8 +445,71 @@
                           {:folder-candidates-count folder-candidates-count
                            :folder-candidates       folder-candidates})))))
 
+(defn choose-panel-folder-title
+  "Returns the title of the folder the given [[panels]] are located in, if all
+   the given [[panels]] are located in the same folder.  If the panels are
+   located in more then one folder this function throws an exception."
+  [panels]
+  (let [panels-by-folder (->> panels
+                              (map #(assoc %
+                                           :folder-identification
+                                           {:title (get-in % ["meta" "folderName"])
+                                            :uid   (get-in % ["meta" "folderUid"])}))
+                              (group-by :folder-identification))
+        panels-folders   (keys panels-by-folder)
+
+        all-panels-located-in-same-folder? (->> panels-folders
+                                                (distinct)
+                                                (count)
+                                                (= 1))]
+    (if all-panels-located-in-same-folder?
+      (:title (first panels-folders))
+      (throw (ex-info (str "The panels are located in different folders: "
+                           (pr-str panels-folders))
+                      {:panels           panels
+                       :panels-by-folder panels-by-folder})))))
+
+(defn choose-alert-folder-uid
+  "Returns the uid of the folder the given [[alerts]] are located in, if all
+   the given [[alerts]] are located in the same folder.  If the alerts are
+   located in more then one folder this function throws an exception."
+  [alerts]
+  (let [alerts-by-folder (->> alerts
+                              (map #(assoc %
+                                           :folder-identification
+                                           {:uid (get % "folderUID")}))
+                              (group-by :folder-identification))
+        alerts-folders   (keys alerts-by-folder)
+
+        all-alerts-located-in-same-folder? (->> alerts-folders
+                                                (distinct)
+                                                (count)
+                                                (= 1))]
+    (if all-alerts-located-in-same-folder?
+      (:uid (first alerts-folders))
+      (throw (ex-info (str "The alerts are located in different folders: "
+                           (pr-str alerts-folders))
+                      {:alerts           alerts
+                       :alerts-by-folder alerts-by-folder})))))
+
 (defn convenient-copy
-  "TODO: document this fn"
+  "Copy a dashboard titled [[dashboard-title]] from [[from-grafana-instance]] to
+   [[to-grafana-instance]] conveniently.  Also copy the library panels and
+   alerts the dashboard depends on.  The convenience this function provides
+   consists of several simple heuristics: 1. It tries to find a dashboard titled
+   [[dashboard-title]] on the given [[from-grafana-instance]].  2. It tries to
+   identify the folder to copy the dashboard to using the name of the folder the
+   dashboard titled [[dashboard-title]] is located on the
+   [[from-grafana-instance]]. See the fn [[choose-folder-uid]] for details.  3.
+   It creates the folder to copy the dashboard to, if it does not find one on
+   [[to-grafana-instance]], which has the same title as on the
+   [[from-grafana-instance]].  4. It checks if the library panels and alerts the
+   dashboard depends on are all located in the same folder. See
+   [[choose-panel-folder-tiele]] and [[choose-alert-folder-uid]] for details.
+   5. If the folders where the library panels and alerts are located in
+   [[from-grafana-instance]] do not exist on [[to-grafana-instance]], they are
+   created using the title of the folders on [[from-grafana-instance]]. See
+   [[choose-folder-uid]] for details."
   [from-grafana-instance to-grafana-instance dashboard-title & {:as   _options
                                                                 :keys [to-message
                                                                        to-board-folder-uid
@@ -439,25 +524,58 @@
                                  (:token from-grafana-instance)
                                  dashboard-uid))
         dashboard-folder-title (get-in dashboard-response ["meta" "folderTitle"])
+        ;; NOTE: Since the alerts and panels "normally" live in the same folder as the
+        ;; dashboard itself. This is due to access permissions, since access is managed using folders.
+        ;; So it seems like we can use the dashboard-folder also as the target
+        ;; for the panels and alerts.
+        ;; Anyways this is not enough, we should check if the folder with the same
+        ;; title exists on the target grafana instance and if the found folder
+        ;; uid is the same as the dashboard-folder-uid
+        ;; BUT: What to do if the checks fail?
         dashboard-folder-uid   (or to-board-folder-uid
                                    (choose-folder-uid to-grafana-instance
                                                       dashboard-folder-title))
-        clean-board-data       (-> dashboard-response
-                                   (get "dashboard")
-                                   (dissoc "version" "id"))
         message                (or to-message
-                                   (str "Copy " (:title dashboard-response)
-                                        " (uid: " (:uid dashboard-response) ") "
+                                   (str "Copy " (get dashboard-response "title")
+                                        " (uid: " (get dashboard-response "uid") ") "
                                         "from " (:url from-grafana-instance)
                                         "to " dashboard-folder-title
                                         " (uid: " dashboard-folder-uid ") "
-                                        "at " (:url to-grafana-instance) "."))]
-    (api/create-update-dashboard (-> to-grafana-instance :url)
-                                 (-> to-grafana-instance :token)
-                                 (helper/clj->json {"dashboard" clean-board-data
-                                                    "message"   message
-                                                    "overwrite" true
-                                                    "folderUid" dashboard-folder-uid}))))
+                                        "at " (:url to-grafana-instance) "."))
+
+        panels                (find-dashboard-related-panels from-grafana-instance dashboard-uid)
+        has-dependent-panels? (not-empty panels)
+        panels-folder-title   (when has-dependent-panels? (choose-panel-folder-title panels))
+        panels-folder-uid     (when has-dependent-panels? (choose-folder-uid to-grafana-instance
+                                                                             panels-folder-title))
+
+        source-alerts              (find-dashboard-related-alert-rules from-grafana-instance dashboard-uid)
+        has-dependent-alerts?      (not-empty source-alerts)
+        source-alerts-folder-uid   (when has-dependent-alerts? (choose-alert-folder-uid source-alerts))
+        source-alerts-folder       (when has-dependent-alerts?
+                                     (api/get-folder-by-folder-uid (-> from-grafana-instance :url)
+                                                                   (-> from-grafana-instance :token)
+                                                                   source-alerts-folder-uid))
+        source-alerts-folder-title (when has-dependent-alerts?
+                                     (get (helper/json->clj source-alerts-folder) "title"))
+        target-alerts-folder-uid   (when has-dependent-alerts?
+                                     (choose-folder-uid to-grafana-instance
+                                                        source-alerts-folder-title))]
+    (when has-dependent-panels?
+      (copy-panels from-grafana-instance
+                   to-grafana-instance
+                   dashboard-uid
+                   panels-folder-uid))
+    (copy-dashboard from-grafana-instance
+                    to-grafana-instance
+                    dashboard-uid
+                    dashboard-folder-uid
+                    message)
+    (when has-dependent-alerts?
+      (copy-alerts from-grafana-instance
+                   to-grafana-instance
+                   dashboard-uid
+                   target-alerts-folder-uid))))
 
 ;; <<< COPY
 
