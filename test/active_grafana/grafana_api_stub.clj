5f2012356c37f375dbaff32b8da47dec3acb27de
Tobias Gutekunst <tobias.gutekunst@active-group.de>
2026-06-16 09:07:07 +0200

    Extend api stub.

--- test/active_grafana/grafana_api_stub.clj
+++ test/active_grafana/grafana_api_stub.clj
@@ -5,3 +5,24 @@
 
 (defn find-dashboards-by-query [case-accessor]
   (constantly (get examples/find-dashboards-by-query-responses case-accessor)))
+
+(def get-dashboard-by-uid (constantly examples/get-dashboard-by-uid-response))
+
+(defn find-folders-by-query [case-accessor]
+  (constantly (get examples/find-folders-by-query-responses case-accessor)))
+
+(def create-folder (constantly examples/create-folder-response))
+
+(def get-library-element-by-uid (constantly examples/get-library-panel-by-uid-response))
+
+(def get-all-alert-rules (constantly examples/get-all-alert-rules-response))
+
+(def get-folder-by-folder-uid (constantly examples/get-folder-by-folder-uid-response))
+
+(def get-library-panels (constantly examples/get-library-panels-response))
+
+(def create-alert-rule (constantly examples/create-alert-rule-response))
+
+(def update-alert-rule (constantly examples/update-alert-rule-response))
+
+(def create-update-dashboard (constantly examples/create-update-dashboard-response))
