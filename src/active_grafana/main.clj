407118fa5a61e676f489fe9b035905924b0546c9
Tobias Gutekunst <tobias.gutekunst@active-group.de>
2026-07-10 09:48:15 +0200

    main: make --help work

--- src/active_grafana/main.clj
+++ src/active_grafana/main.clj
@@ -1,7 +1,10 @@
 (ns active-grafana.main
-  (:require [babashka.cli :as cli]
-            [active-grafana.core :as core]
-            [active-grafana.settings :as settings]))
+  (:require [active-grafana.core :as core]
+            [active-grafana.settings :as settings]
+            ;; NOTE: reload babashka.cli because of
+            ;; the version built-in to babashka does not support --help
+            ;; see https://github.com/babashka/babashka/issues/1984#issuecomment-4810338306
+            [babashka.cli :as cli] :reload))
 
 ;; TODO: Read throug https://blog.michielborkent.nl/babashka-cli-help-and-completions.html
 ;; More resources:
@@ -9,6 +12,7 @@
 ;; https://github.com/babashka/cli
 ;; https://www.braveclojure.com/quests/babooka/
 ;; https://book.babashka.org/
+;; https://github.com/babashka/babashka/blob/master/doc/projects.md
 
 (defn copy [{:keys [opts]}]
   (let [source-grafana-instance (settings/make-source-grafana-instance opts)
@@ -18,30 +22,28 @@
                           target-grafana-instance
                           dashboard-title)))
 
-(def cli-opts
-  {:convenient           {:coerce :boolean
-                          :desc "A"}
-   :source-url           {:desc    "URL of the source grafana instance to copy the dashboard from."
-                          :require true}
-   :source-token         {:desc    "Token to authenticate against the source grafana instance."
-                          :require true}
-   :target-url           {:desc    "URL of the target grafana instance to copy the dashboard to."
-                          :require true}
-   :target-token         {:desc    "Token to authenticate against the target grafana instance."
-                          :require true}
-   :title                {:desc    "The title of the dashboard to copy on the source grafana instance."
-                          :require true}
-
-   ;; :message              {:desc "An optional message to use as commit message for the updated/created dashboard on the target grafana instance."}
-   ;; :target-folder-uid    {:desc "An optional target folder uid used to copy the dashboard to on the target grafana instance."}
-   ;; :source-dashboard-uid {:desc "An optional dashboard uid to use instead of using "}
-   })
-
-(defn help [_]
-  (println "TODO: read the babashka-cli-help-and-completions blog post. See the top of src/active_grafana/main.clj"))
-
 (def table
-  [{:cmds ["copy"] :fn copy :spec cli-opts}
-   {:cmds [] :fn help}])
+  [{:cmds ["copy"]
+    :fn copy
+    :doc "Copy a file\nMore details here"
+    :spec {:convenient           {:coerce :boolean
+                                  :desc "A"}
+           :source-url           {:desc    "URL of the source grafana instance to copy the dashboard from."
+                                  :require true}
+           :source-token         {:desc    "Token to authenticate against the source grafana instance."
+                                  :require true}
+           :target-url           {:desc    "URL of the target grafana instance to copy the dashboard to."
+                                  :require true}
+           :target-token         {:desc    "Token to authenticate against the target grafana instance."
+                                  :require true}
+           :title                {:desc    "The title of the dashboard to copy on the source grafana instance."
+                                  :require true}
+           ;; :message              {:desc "An optional message to use as commit message for the updated/created dashboard on the target grafana instance."}
+           ;; :target-folder-uid    {:desc "An optional target folder uid used to copy the dashboard to on the target grafana instance."}
+           ;; :source-dashboard-uid {:desc "An optional dashboard uid to use instead of using "}
+           }}])
 
-(cli/dispatch table *command-line-args*)
+(defn -main [& args]
+  (cli/dispatch table args {:prog "active-grafana" :help true}))
+
+(apply -main *command-line-args*)
